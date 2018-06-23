ec1f85718cf6c068854c1a62d8f393e3e70a07d4
c6218downswallow <masato59637@yahoo.co.jp>
2018-06-23 12:17:24 +0900

    RPGfile

---
+++ Assets/Scripts/build.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class build : MonoBehaviour {
+    public GameObject fall01;
+    public GameObject lake01;
+    Dictionary<char, GameObject> map;
+    public TextAsset text;
+	void Start () {
+        map = new Dictionary<char, GameObject>()
+        {
+            {'f',fall01 },//'f'を壁オブジェクトに関連付ける
+            {'l',lake01 },
+            {'\n',null },
+        };
+        Stagebuild(text.text);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    public void Stagebuild(string text)
+    {
+        if (text == null)
+        {
+            Debug.Log("error!");
+            return;
+        }
+        Vector3 originpos = new Vector3(0, 0, 0);
+        Vector3 pos = originpos;
+        GameObject obj = map['f'];
+        foreach(char c in text)
+        {
+            if (c != '\n')
+            {
+                if (map.ContainsKey(c))
+                {
+                    obj = Instantiate(map[c], pos, Quaternion.identity);
+                    obj.name = map[c].name;
+                }
+                pos.x += 0.32f;
+            }
+            else
+            {
+                pos.y -= 0.3f ;
+                pos.x = originpos.x;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/tikei.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tikei : MonoBehaviour {
+    public int type;
+    //typeの値　種類
+    //   0       壁
+    //　１　　　壁（破壊可能）
+    //　２　　　回復系地形
+    //　３　　　ダメージ系地形
+    public void breaking()
+    {
+        if (type == 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
