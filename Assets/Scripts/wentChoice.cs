2026233393baa4e7213a0e57b838a090fe30e59a
c6218explessswallow <masato59637@yahoo.co.jp>
2018-08-09 20:14:17 +0900

    20180809の更新

---
+++ Assets/Scripts/wentChoice.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wentChoice : MonoBehaviour {
+    void Toroad()
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel("Loaddata");
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
