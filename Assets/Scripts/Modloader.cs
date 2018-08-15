6542cf20d369a1430e31a7528166010ef881028c
c6218explessswallow <masato59637@yahoo.co.jp>
2018-08-15 17:42:59 +0900

    外部ファイrの読み込みテスト

---
+++ Assets/Scripts/Modloader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Modloader : MonoBehaviour {
+    string path;
+	// Use this for initialization
+	void Start () {
+        path = Application.dataPath + "/../../Stage";
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        DirectoryInfo dir = new DirectoryInfo(path);
+        FileInfo[] info = dir.GetFiles("*.txt");
+        Debug.Log("load data!");
+        foreach(FileInfo f in info)
+        {
+            Debug.Log(f.Name);
+        }
+        Debug.Log("have loaded data!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
