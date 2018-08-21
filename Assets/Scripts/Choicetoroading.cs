e284d51a7eff2e03d7b2eb96bb621c918a023247
c6218explessswallow <masato59637@yahoo.co.jp>
2018-08-21 23:44:17 +0900

    You can choice saved datas.

---
+++ Assets/Scripts/Choicetoroading.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Choicetoroading : MonoBehaviour {
+    public Text[] texts;
+    int choice = 0;
+    private void Update()
+    {
+        for(int i = 0; i < texts.Length;i++)
+        {
+            if (choice == i)
+            {
+                texts[i].color = Color.green;
+            }
+            else
+            {
+                texts[i].color = Color.yellow;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (choice != 0)
+            {
+                choice--;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (choice != (texts.Length - 1))
+            {
+                choice++;
+            }
+        }
+    }
+}
