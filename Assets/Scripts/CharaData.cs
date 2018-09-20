199912684838055c34678f5ff12915a13e26bcb0
c6218explessswallow <masato59637@yahoo.co.jp>
2018-09-20 20:37:33 +0900

    久しぶりの更新

---
+++ Assets/Scripts/CharaData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CharaData : ScriptableObject {
+    public int hp;
+    public bool condition1;
+    public bool condition2;
+    public bool condition3;
+    public bool condition4;
+    public int mp;
+}
---
+++ Assets/Scripts/Itemdata.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu]
+public class Itemdata : MonoBehaviour {
+    public Image ItemChip;//アイテム画像
+    public string infomation;//説明
+    public int many;//いくつ所持しているか
+    public int heal;//使用時のHP回復量
+    public int Damage;//使用時の敵へのダメージ
+    public int Condision;//特殊効果フラグ
+    public bool Checker()
+    {
+        return !(many <= 0);//アイテムを所持しているか（イベントフラグに使用？）
+    }
+}
